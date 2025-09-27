ea9d3bcf44b4cb2db1721723ff660ddbc95ac132
retrogamesets <mike.bosche@laposte.net>
2025-09-27 16:09:26 +0200

    update ext

--- data/rgsx_sources_manager.php
+++ data/rgsx_sources_manager.php
@@ -993,7 +993,7 @@ $action = $_POST['action'] ?? '';
 $activeTab = $_POST['active_tab'] ?? $_GET['active_tab'] ?? $_SESSION['active_tab'] ?? 'tab-scrape';
 $message = '';
 $error = '';
-$allowedExtensions = ['xci','zip','rar','7z','tar','gz','chd','txt','wsquashfs','iso','torrent'];
+$allowedExtensions = ['xci','nsp','zip','rar','7z','tar','gz','chd','wsquashfs','iso','wbfs','nds','dsi','cia','3ds','cia','gba','gbc','gb','nes','sfc','smc','gen','md','sms','gg','pce','sgx','a26','a78','bin','cue','img','pbp','rom','n64'];
 
 try {
   switch ($action) {
